c3130bde67b8db86f9e6dff9e2377a03ae60b4a3
Stephan Hövelbrinks <stephan.hoevelbrinks@craftinglegends.com>
2018-10-23 10:24:51 +0200

    added button

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
-function display() {
-    alert("Hello World!");
+function display() {    
+    var element = document.getElementById("result");
+    var randomNumber = Math.random() * 100;
+    element.innerHTML = "clicked: " + randomNumber;
  }
